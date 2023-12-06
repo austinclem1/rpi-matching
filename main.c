ec6b0c82b94a83ff9af77a9564ec578c335d0302
Austin Clements <austin.clements@smoothstack.com>
2023-12-06 10:56:29 -0600

    add more input handling code

--- main.c
+++ main.c
@@ -9,35 +9,42 @@ typedef enum {
     NUM_CHOICES,
 } Choice;
 
-static const int ledPins[NUM_CHOICES] = { ?, ?, ? };
-static const int freqs[NUM_CHOICES] = { 440, 550, 660 };
-static const int buttonPins[NUM_CHOICES] = { ?, ?, ? };
+typedef enum {
+    event_button_down,
+    event_button_up,
+} EventType;
 
 typedef enum {
     game_state_playback,
     game_state_input,
+    game_state_game_over,
 } GameState;
+
+static const int ledPins[NUM_CHOICES] = { ?, ?, ? };
+static const int freqs[NUM_CHOICES] = { 440, 550, 660 };
+static const int buttonPins[NUM_CHOICES] = { ?, ?, ? };
 
 void startTone(Choice choice) {
     const int freq = freqs[choice];
     const int period_ns = NS_PER_SEC / freq;
     const int duty_cycle_ns = period_ns / 2;
 }
+
 void stopTone() {}
 
-void turnOnLed(Choice choice) {
-    // TODO clear all leds
-    // set led ledPins[choice] on
-}
+void turnOnLed(Choice choice) {}
 
-void turnOffLeds() {}
+void turnOffAllLeds() {}
+
+bool pollInput() {}
 
 int main(int argc, char **argv) {
     int cur_time, last_time, elapsed_time;
     Choice sequence[MAX_SEQUENCE_LEN];
     int sequence_len;
-    int playback_time_remaining;
     int cur_sequence_index;
+    int playback_time_remaining;
+    bool waiting_for_button_release;
     GameState game_state;
     bool running;
     
@@ -54,6 +61,7 @@ int main(int argc, char **argv) {
     while (running) {
         cur_time = timeNow();
         elapsed_time = cur_time - last_time;
+        last_time = cur_time;
         
         switch (game_state) {
         case game_state_playback:
@@ -64,18 +72,46 @@ int main(int argc, char **argv) {
                     stopTone();
                     turnOffLeds();
                     clearInputEvents();
+                    waiting_for_button_release = false;
                     game_state = game_state_input;
                 } else {
-                    Choice cur_playback_choice = sequence[cur_sequence_index];
-                    startTone(cur_playback_choice);
-                    turnOnLed(cur_playback_choice);
+                    startTone(sequence[cur_sequence_index]);
+                    turnOffLeds();
+                    turnOnLed(sequence[cur_sequence_index]);
                     playback_time_remaining = DEFAULT_PLAYBACK_DELAY;
                 }
             }
             break;
         case game_state_input:
-            const InputEvent input = pollInput();
-            break;
+            const InputEvent input_event;
+            if (pollInput(&input_event)) {
+                switch (input_event.type) {
+                case event_button_down:
+                    if (waiting_for_button_release) break;
+                    if (input_event.choice == sequence[cur_sequence_index]) {
+                        startTone(sequence[cur_sequence_index]);
+                        turnOffLeds();
+                        turnOnLed(sequence[cur_sequence_index]);
+                        waiting_for_button_release = true;
+                    } else {
+                        game_state = game_state_game_over;
+                    }
+                    break;
+                case event_button_up:
+                    if (waiting_for_button_release && input_event.choice == sequence_len[cur_sequence_index]) {
+                        stopTone();
+                        turnOffLeds();
+                        waiting_for_button_release = false;
+                        cur_sequence_index++;
+                        if (cur_sequence_index >= sequence_len) {
+                            sequence[sequence_len] = getRandom() % NUM_CHOICES;
+                            sequence_len++;
+                            game_state = game_state_playback;
+                        }
+                    }
+                    break;
+                } 
+            }
         }
     }
     
